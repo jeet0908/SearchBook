c2b68ae7304648a5ee561a8a8726bc3225f515f2
jeet0908 <45686171+jeet0908@users.noreply.github.com>
2019-04-03 14:18:40 +0530

    written two test cases..will add up more

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe("App", () => {
+  it("should render App component", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(".App").length).toBe(1);
+  });
+});
+
+it("Should show state value on change event in App component", () => {
+   const component = shallow(<App />);
+   const inputValue = 'harryPotter';
+    const form = component.find('FormControl');
+    // when
+    form.props().onChange({target: {
+       value: inputValue
+    }});
+    // then
+    expect(component.state('query')).toBe(inputValue);
 });
